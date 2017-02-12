988e4390cb59a586174b552bbeb2c72f51450b30
Benjamin Croker <ben@ben-macbook.local>
2017-02-12 23:22:40 +1000

    Change case formatting of named tuple attributes. Add quality_method to reading rows

--- nemreader/nem_reader.py
+++ nemreader/nem_reader.py
@@ -6,7 +6,7 @@ from collections import namedtuple
 
 Reading = namedtuple(
     'Reading',
-    ['reading_start', 'reading_end', 'reading_value', 'UOM']
+    ['reading_start', 'reading_end', 'reading_value', 'UOM', 'quality_method']
 )
 
 
@@ -27,11 +27,11 @@ class MeterRecord(object):
                     if row[0] != '100':
                         logging.warning('NEM Files must start with a 100 row')
 
-                    HeaderRecord = parse_100_row(row)
-                    self.version_header = HeaderRecord.VersionHeader
-                    self.file_created = HeaderRecord.DateTime
-                    self.file_created_by = HeaderRecord.FromParticipant
-                    self.file_created_for = HeaderRecord.ToParticipant
+                    header_record = parse_100_row(row)
+                    self.version_header = header_record.version_header
+                    self.file_created = header_record.datetime
+                    self.file_created_by = header_record.from_participant
+                    self.file_created_for = header_record.to_participant
                     continue
 
                 record_indicator = int(row[0])
@@ -40,18 +40,18 @@ class MeterRecord(object):
 
                 if self.version_header == 'NEM12':
                     if record_indicator == 200:
-                        MeterData = parse_200_row(row)
-                        self.NMI = MeterData.NMI
-                        self.NMI_configuration = MeterData.NMIConfiguration
-                        uom = MeterData.UOM
-                        interval = MeterData.IntervalLength
-                        nmi_suffix = MeterData.NMISuffix
+                        meter_data = parse_200_row(row)
+                        self.NMI = meter_data.NMI
+                        self.NMI_configuration = meter_data.NMI_configuration
+                        uom = meter_data.UOM
+                        interval = meter_data.interval_length
+                        nmi_suffix = meter_data.NMI_suffix
                         if nmi_suffix not in self.readings:
                             self.readings[nmi_suffix] = []
 
                     if record_indicator == 300:
-                        IntervalRecord = parse_300_row(row, interval, uom)
-                        for reading in IntervalRecord.IntervalValues:
+                        interval_record = parse_300_row(row, interval, uom)
+                        for reading in interval_record.interval_values:
                             self.readings[nmi_suffix].append(reading)
 
                 elif self.version_header == 'NEM13':
@@ -70,14 +70,15 @@ class MeterRecord(object):
                     logging.warning(msg)
 
 
-def calculate_manual_reading(BasicMeterData):
+def calculate_manual_reading(basic_meter_data):
     """ Calculate the interval between two manual readings
     """
-    reading_start = BasicMeterData.PreviousRegisterReadDateTime
-    reading_end = BasicMeterData.CurrentRegisterReadDateTime
-    value = BasicMeterData.CurrentRegisterRead - BasicMeterData.PreviousRegisterRead
-    uom = BasicMeterData.UOM
-    return Reading(reading_start, reading_end, value, uom)
+    reading_start = basic_meter_data.PreviousRegisterReadDateTime
+    reading_end = basic_meter_data.CurrentRegisterReadDateTime
+    value = basic_meter_data.CurrentRegisterRead - basic_meter_data.PreviousRegisterRead
+    uom = basic_meter_data.UOM
+    quality_method = basic_meter_data.CurrentQualityMethod
+    return Reading(reading_start, reading_end, value, uom, quality_method)
 
 
 def parse_100_row(row):
@@ -85,10 +86,10 @@ def parse_100_row(row):
     """
     HeaderRecord = namedtuple(
         'HeaderRecord',
-        ['VersionHeader',
-         'DateTime',
-         'FromParticipant',
-         'ToParticipant']
+        ['version_header',
+         'datetime',
+         'from_participant',
+         'to_participant']
     )
     return HeaderRecord(row[1],
                         parse_datetime(row[2], '%Y%m%d%H%M'),
@@ -103,14 +104,14 @@ def parse_200_row(row):
     MeterData = namedtuple(
         'MeterData',
         ['NMI',
-         'NMIConfiguration',
-         'RegisterID',
-         'NMISuffix',
-         'MDMDataStreamIdentifier',
-         'MeterSerialNumber',
+         'NMI_configuration',
+         'register_id',
+         'NMI_suffix',
+         'MDM_datastream_identitfier',
+         'meter_serial_number',
          'UOM',
-         'IntervalLength',
-         'NextScheduledReadDate'
+         'interval_length',
+         'next_scheduled_read_date'
          ]
     )
 
@@ -189,19 +190,22 @@ def parse_300_row(row, interval=30, uom='kWh'):
     num_intervals = int(24 / (interval / 60))
     interval_date = parse_datetime(row[1], '%Y%m%d')
     last_interval = 2 + num_intervals
+    quality_method = row[last_interval]
+
     interval_values = parse_intervals(
-        row[2:last_interval], interval_date, interval, uom)
+        row[2:last_interval], interval_date, interval, uom, quality_method
+    )
 
     IntervalRecord = namedtuple(
         'IntervalRecord',
         [
-            'IntervalDate',
-            'IntervalValues',
-            'QualityMethod',
-            'ReasonCode',
-            'ReasonDescription',
-            'UpdateDateTime',
-            'MSATSLoadDateTime'
+            'interval_data',
+            'interval_values',
+            'quality_method',
+            'reason_code',
+            'reason_description',
+            'update_datetime',
+            'MSATS_load_datatime'
         ]
     )
 
@@ -222,11 +226,11 @@ def parse_400_row(row):
     EventRecord = namedtuple(
         'IntervalRecord',
         [
-            'StartInterval',
-            'EndInterval',
-            'QualityMethod',
-            'ReasonCode',
-            'ReasonDescription'
+            'start_interval',
+            'end_interval',
+            'quality_method',
+            'reason_code',
+            'reason_description'
         ]
     )
 
@@ -238,7 +242,7 @@ def parse_400_row(row):
                        )
 
 
-def parse_intervals(rows, interval_date, interval, uom):
+def parse_intervals(rows, interval_date, interval, uom, quality_method):
     """ Convert interval values into tuples with datetime
     """
 
@@ -246,7 +250,7 @@ def parse_intervals(rows, interval_date, interval, uom):
     for i, row in enumerate(rows):
         reading_start = interval_date + (i * interval_delta)
         reading_end = interval_date + (i * interval_delta) + interval_delta
-        yield Reading(reading_start, reading_end, float(row), uom)
+        yield Reading(reading_start, reading_end, float(row), uom, quality_method)
 
 
 def parse_datetime(record, date_format='%Y%m%d%H%M%S'):
