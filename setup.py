1a2a2e5fd99fe923e9631de0a094a3fc45352702
A Guinane <alexguinane@gmail.com>
2017-02-24 20:47:24 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'nemreader',
+    packages = ['nemreader'],
+    version = '0.1.1',
+    description = 'Parse NEM12 (interval metering data) and NEM13 (accumulated metering data) data files ',
+    author = 'aguinane',
+    author_email = 'alexguinane@gmail.com',
+    url = 'https://github.com/aguinane/nem-reader',
+    keywords = ['energy', 'NEM12', 'NEM12'],
+    classifiers = [],
+    license = 'MIT',
+)
