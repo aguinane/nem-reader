78801b52b1ea4013c6ee3c9b6a825657878093fe
david_perry <dperry.aus@gmail.com>
2020-02-11 14:39:37 +1100

    Handle quirks in NEM12 format exported by Powercor/Citipower customer portal. Handle missing optional fields.

---
+++ tests/test_powercor.py
@@ -0,0 +1,45 @@
+""" Test Suite
+"""
+
+import pytest
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import nemreader as nr
+
+
+def test_correct_NMIs():
+    meter_data = nr.read_nem_file("examples/unzipped/Example_NEM12_powercor.csv", ignore_missing_header=True)
+    assert len(meter_data.readings) == 1
+    assert "VABD000163" in meter_data.readings
+
+
+def test_correct_channels():
+    meter_data = nr.read_nem_file("examples/unzipped/Example_NEM12_powercor.csv", ignore_missing_header=True)
+    readings = meter_data.readings["VABD000163"]
+    assert len(readings) == 2
+    assert "E1" in readings
+    assert "Q1" in readings
+
+
+def test_correct_records():
+    meter_data = nr.read_nem_file("examples/unzipped/Example_NEM12_powercor.csv", ignore_missing_header=True)
+    readings = meter_data.readings["VABD000163"]
+
+    assert len(readings["E1"]) == 96
+    assert readings["E1"][10].read_value == pytest.approx(1.11, 0.1)
+    assert readings["E1"][-1].read_value == pytest.approx(3.33, 0.1)
+
+    assert len(readings["Q1"]) == 96
+    assert readings["Q1"][10].read_value == pytest.approx(2.22, 0.1)
+    assert readings["Q1"][-1].read_value == pytest.approx(4.44, 0.1)
+    assert readings["Q1"][-1].quality_method == "A"
+
+
+def test_zipped_load():
+    meter_data = nr.read_nem_file("examples/nem12/Example_NEM12_powercor.csv.zip", ignore_missing_header=True)
+
+
+def test_missing_fields():
+    meter_data = nr.read_nem_file("examples/unzipped/Example_NEM12_powercor.csv", ignore_missing_header=True)
