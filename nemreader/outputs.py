590a20656ed6410c237adce5b156d58497c69771
Alex Guinman <alex@guinman.id.au>
2021-10-10 16:47:34 +1000

    Use pandas to flatten rows to ensure times match

--- nemreader/outputs.py
+++ nemreader/outputs.py
@@ -9,6 +9,7 @@ import logging
 import csv
 from typing import Generator, Tuple, List, Dict, Any
 from pathlib import Path
+import pandas as pd
 from .nem_objects import Reading
 from .nem_reader import read_nem_file
 from .split_days import split_multiday_reads
@@ -24,12 +25,12 @@ def nmis_in_file(file_name) -> Generator[Tuple[str, List[str]], None, None]:
         yield nmi, suffixes
 
 
-def flatten_rows(
+def get_data_frame(
     nmi_transactions: Dict[str, list],
     nmi_readings: Dict[str, List[Reading]],
     split_days: bool = False,
-) -> Tuple[List[str], List[list]]:
-    """Create flattened list of NMI reading data"""
+) -> pd.DataFrame:
+    """Get a Pandas DataFrame for Point(s)"""
 
     channels = list(nmi_transactions.keys())
     if split_days:
@@ -37,50 +38,39 @@ def flatten_rows(
         for ch in channels:
             nmi_readings[ch] = list(split_multiday_reads(nmi_readings[ch]))
 
-    headings = ["period_start", "period_end"]
-    for channel in channels:
-        headings.append(channel)
-    headings.append("quality_method")
-    headings.append("event")
-
-    rows = []
-    num_records = len(nmi_readings[channels[0]])
     first_ch = channels[0]
-    for i in range(0, num_records):
-        t_start = nmi_readings[first_ch][i].t_start
-        t_end = nmi_readings[first_ch][i].t_end
-        quality_method = nmi_readings[first_ch][i].quality_method
-        event_code = nmi_readings[first_ch][i].event_code
-        event_desc = nmi_readings[first_ch][i].event_desc
-        row: List[Any] = [t_start, t_end]
-        for ch in channels:
-            try:
-                val = nmi_readings[ch][i].read_value
-            except IndexError:
-                val = None
-            row.append(val)
-        row.append(quality_method)
-        row.append(f"{event_code} {event_desc}")
-        rows.append(row)
-    return headings, rows
+    d = {
+        "t_start": [x.t_start for x in nmi_readings[first_ch]],
+        "t_end": [x.t_end for x in nmi_readings[first_ch]],
+        "quality_method": [x.quality_method for x in nmi_readings[first_ch]],
+        "event_code": [x.event_code for x in nmi_readings[first_ch]],
+        "event_desc": [x.event_desc for x in nmi_readings[first_ch]],
+    }
+    d[first_ch] = [x.read_value for x in nmi_readings[first_ch]]
+
+    df = pd.DataFrame(data=d, index=d["t_start"])
+
+    for ch in channels[1:]:
+        index = [x.t_start for x in nmi_readings[ch]]
+        values = [x.read_value for x in nmi_readings[ch]]
+        ser = pd.Series(data=values, index=index, name=ch)
+
+        df.loc[:, ch] = ser
+    return df
 
 
 def output_as_data_frames(
     file_name, split_days: bool = True, ignore_missing_header: bool = False
-):
+) -> List[pd.DataFrame]:
     """Return list of data frames for each NMI"""
-
-    import pandas as pd
 
     m = read_nem_file(file_name, ignore_missing_header=ignore_missing_header)
     nmis = list(m.readings.keys())
     data_frames = []
     for nmi in nmis:
         nmi_readings = m.readings[nmi]
-        headings, rows = flatten_rows(m.transactions[nmi], nmi_readings, split_days)
-        nmi_df = pd.DataFrame(data=rows, columns=headings)
+        nmi_df = get_data_frame(m.transactions[nmi], nmi_readings, split_days)
         data_frames.append((nmi, nmi_df))
-
     return data_frames
 
 
@@ -100,11 +90,11 @@ def output_as_csv(file_name, output_dir="."):
     m = read_nem_file(file_name)
     nmis = m.readings.keys()
     for nmi in nmis:
-        headings, rows = flatten_rows(m.transactions[nmi], m.readings[nmi])
-        last_date = rows[-1][1]
+        df = get_data_frame(m.transactions[nmi], m.readings[nmi])
+        last_date = df.iloc[-1][1]
         output_file = "{}_{}_transposed.csv".format(nmi, last_date.strftime("%Y%m%d"))
         output_path = output_dir / output_file
-        save_to_csv(headings, rows, output_path)
+        df.to_csv(output_path, index=False)
         output_paths.append(output_path)
     return output_paths
 
