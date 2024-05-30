8b56afa86c88a9a2aa42c908fb49ae3a9f91c6cc
Alex Guinman <alex@guinman.id.au>
2024-05-30 18:44:23 +1000

    Fix edge case and add test

--- tests/test_dataframe_pivot.py
+++ tests/test_dataframe_pivot.py
@@ -21,3 +21,17 @@ def test_pivot_dataframe():
     for suffix in ["t_start", "E1", "E2", "V1", "quality", "evt_code", "evt_desc"]:
         assert suffix in df.columns
     assert len(df) == 48
+
+
+def test_pivot_dataframe_partialchannel():
+    """Test example where some days are missing from first channel in file"""
+    nf = NEMFile("examples/unzipped/Example_NEM12_partialchannel.csv", strict=True)
+
+    # The intervals are different, so we have 48 30min + 144 10min = 192 rows
+    df = nf.get_pivot_data_frame(set_interval=30)
+    for suffix in ["t_start", "B1", "E1", "quality", "evt_code", "evt_desc"]:
+        assert suffix in df.columns
+    assert len(df) == 1488
+    for col in ["t_start", "E1", "quality", "evt_code", "evt_desc"]:  # only B1 has nans
+        perc_nans = df[col].isna().sum() / len(df[col])
+        assert perc_nans == 0.0
